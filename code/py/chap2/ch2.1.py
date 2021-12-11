85d42df834e267fda9b940255689e550f0ca835e
Bing Long <bing_long@yahoo.com>
2021-12-10 19:33:06 -0800

    generate

---
+++ code/py/chap2/ch2.1.py
@@ -0,0 +1,14 @@
+\begin{spacing}{0.8}
+\begin{lstlisting}[language=Python]
+
+import re
+
+x = '100以内有25个质数。' + \
+  '不实际计算的话,其中51和91很容易被误判为是质数。'
+x = re.sub('质数', '素数', x)
+x = x.replace('其中', '')
+print(x)
+
+
+\end{lstlisting}
+\end{spacing}
