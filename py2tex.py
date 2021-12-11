ba71d4b987eb27a2049363f05d7cb79d85437b01
Bing Long <bing_long@yahoo.com>
2021-12-10 20:19:31 -0800

    py2tex

---
+++ py2tex.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 10 20:13:25 2021
+
+@author: bing
+"""
+import os
+
+curr_dir = os.path.dirname(os.path.abspath(__file__))
+
+print(curr_dir)
