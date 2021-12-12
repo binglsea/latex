21069d9f6109afe9220fccc2bff3132cb1cf6a7b
Bing Long <bing_long@yahoo.com>
2021-12-11 18:06:30 -0800

    IntelliJ/ PyCharm

--- code/py/chap2/ch2.1.py
+++ code/py/chap2/ch2.1.py
@@ -1,6 +1,14 @@
 import re
+import math
 
+#%%
 x = "100以内有25个质数。不实际计算的话,51和91很容易被误判为是质数。"
 y = re.sub("质数", "素数", x)
 z = x.replace("素数", "质数")
 print(x)
+
+#%%
+
+print("hello")
+print(re.sub("This", "That", "This is not"))
+print(math.pi)
