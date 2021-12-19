d6866b5177fed3698bde56db5154fd102984f71e
Bing Long <bing_long@yahoo.com>
2021-12-19 09:20:28 -0800

    numbers

--- code/py/chap5/numbers.py
+++ code/py/chap5/numbers.py
@@ -5,11 +5,9 @@ Created on Sun Dec 12 07:07:44 2021
 
 @author: bing
 """
-from numpy import arange
-from numpy import array
-
-a = arange(2, 12).reshape(2,5)
-print(a)
-print(3*a)
-print(a.dtype)
-print(a[1,:])
+import numpy as np
+import matplotlib.pyplot as plt
+X = np.linspace(-3, 2, 200)
+Y = X ** 2 - 2 * X + 1.
+plt.plot(X, Y)
+plt.show()
